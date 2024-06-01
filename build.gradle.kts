a0af4490b8dda54ea0da5e648f045ae890898c86
Tofpu <tofpuu@gmail.com>
2024-06-01 14:19:33 +0300

    Add `maven-publish` to the parent build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,21 @@
 plugins {
-    id("java")
+    id("maven-publish")
 }
 
-group = "io.tofpu.migratio"
-version = "1.0-SNAPSHOT"
+subprojects {
+    group = "io.tofpu.migratio"
+    version = "1.0-SNAPSHOT"
+
+    apply(plugin = "java-library")
+    apply(plugin = "maven-publish")
+
+    publishing {
+        publications {
+            create<MavenPublication>("maven") {
+                artifactId = project.name
+
+                from(components["java"])
+            }
+        }
+    }
+}
