ddb25e1038221a74f11e107945d2611c9646b7d9
Tofpu <tofpuu@gmail.com>
2024-06-03 18:14:37 +0300

    Allow migration classes to be provided directly to the builder
    Fixes #9

--- migratio-core/src/test/java/io/tofpu/migratio/MigratioBuilderTest.java
+++ migratio-core/src/test/java/io/tofpu/migratio/MigratioBuilderTest.java
@@ -3,24 +3,53 @@ package io.tofpu.migratio;
 import org.junit.jupiter.api.Test;
 
 import java.util.Collection;
+import java.util.Collections;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MigratioBuilderTest {
     @Test
     void alternative_classes_supplier_test() {
-        Migratio.Builder<?> builder = new Migratio.Builder<>("io.tofpu.migratio")
+        TestBuilder builder = new TestBuilder("io.tofpu.migratio")
                 .setClassesSupplier(() -> new Class[0]);
 
-        Collection<Migration> collection = builder.findAndSortMigrations(Migration.class);
+        Collection<TestMigration> collection = builder.findAndSortMigrations(TestMigration.class);
         assertEquals(0, collection.size());
     }
 
+    @Test
+    void migration_supplier_test() {
+        TestBuilder builder = new TestBuilder("io.tofpu.migratio")
+                .setMigrationSupplier(() -> Collections.singletonList(TestMigration.class).toArray(new TestMigration[0]));
+
+        Collection<TestMigration> collection = builder.findAndSortMigrations(TestMigration.class);
+        assertEquals(1, collection.size());
+
+    }
+
+    static class TestBuilder extends Migratio.Builder<TestBuilder, TestMigration> {
+        protected TestBuilder(String packageName) {
+            super(packageName);
+        }
+    }
+
     @SuppressWarnings("unused")
-    static class TestMigration implements Migration<MigrationContext> {
+    public static class TestMigration implements Migration<MigrationContext> {
         @Override
         public String version() {
             return "1.0";
+        }
+
+        @Override
+        public void handle(MigrationContext context) {
+        }
+    }
+
+    @SuppressWarnings("unused")
+    public static class AnotherTestMigration implements Migration<MigrationContext> {
+        @Override
+        public String version() {
+            return "1.1";
         }
 
         @Override
